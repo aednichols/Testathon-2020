2c4e5552f3f379c16dc2fd5b56502b0268bb5e03
Adam Nichols <aednichols@gmail.com>
2020-02-19 16:43:05 -0500

    Fix test_write_json_map

--- pytest-wdl-tests/tests/stdlib/read_write_json/write_json_map/test_write_json_map.py
+++ pytest-wdl-tests/tests/stdlib/read_write_json/write_json_map/test_write_json_map.py
@@ -3,6 +3,6 @@ def test_write_json_map(workflow_runner):
         "write_json_map.wdl",
         {},
         {
-            "result": "{\"hello string\": \"hello\", \"emptystr\": \"\"}"
+            "result": "{\"hello string\":\"hello\",\"emptystr\":\"\"}"
         }
     )
